2e870b51fcd0442b9c549b2e44d5231e76864312
Andres Garcia <jgarciaf106@gmail.com>
2022-05-11 17:31:51 -0600

    Initial Work and Library Results

---
+++ database.js
@@ -0,0 +1,28 @@
+class DataBase {
+  config = {
+    database: "",
+    server: "",
+    port: 0,
+    trustedConn: false,
+  };
+
+  constructor(dbConfig) {
+    this.config = dbConfig;
+  }
+
+  setEngine() {
+    const dbEngine = {
+      database: this.config.database,
+      server: this.config.server,
+      port: this.config.port,
+      options: {
+        trustedConnection: this.config.trustedConn,
+        driver: "msnodesqlv8",
+      },
+    };
+
+    return dbEngine;
+  }
+}
+
+module.exports = DataBase;
--- index.js
+++ index.js
@@ -1,48 +1,5 @@
-const sql = require("mssql/msnodesqlv8");
+const data = require("./data");
+const database = require("./database");
 
-class DataBase {
-  config = {
-    database: "",
-    server: "",
-    port: 0,
-    trustedConn: false,
-  };
-
-  constructor(dbConfig) {
-    this.config = dbConfig;
-  }
-
-  setEngine() {
-    const dbEngine = {
-      database: this.config.database,
-      server: this.config.server,
-      port: this.config.port,
-      options: {
-        trustedConnection: this.config.trustedConn,
-        driver: "msnodesqlv8",
-      },
-    };
-
-    return dbEngine;
-  }
-}
-
-class Data {
-
-  constructor() {
-  }
-
-  run_sql_query = async (table, engine) => {
-    try {
-      await sql.connect(engine);
-      const result = await sql.query(`select * from ${table}`);
-      console.log(result);
-      sql.close();
-    } catch (err) {
-      console.dir(err.recordset);
-    }
-  };
-}
-
-module.exports.DataBase = DataBase;
-module.exports.Data = Data;
+module.exports = data;
+module.exports = database;
